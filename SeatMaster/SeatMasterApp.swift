d43c645a12909dbeb430c4da0566d13f2ac9b4de
Vickers Zhu <vickerszhu@163.com>
2024-05-22 16:53:14 +0900

    Renamed

---
+++ SeatMaster/SeatMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatMasterApp.swift
+//  SeatMaster
+//
+//  Created by Vickers Zhu on 2024/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SeatMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
